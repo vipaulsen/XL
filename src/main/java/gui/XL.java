4fd020353d97731a1caacbeb0fc8dedae65b83ec
Alexnader <alexander.mohe@gmail.com>
2021-04-23 10:00:38 +0200

    maybe

--- src/main/java/gui/XL.java
+++ src/main/java/gui/XL.java
@@ -30,7 +30,7 @@ public class XL extends Application {
   XLModel model = new XLModel();
 
   public XL() {
-    // TODO: add listener(s) for model? No i think i wont
+    // TODO: add listener(s) for model? Okay maybe i will
   }
 
   public void onCellSelected(GridCell cell) {
